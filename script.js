a0e6a84b9fdf8c917ca2386f2224051670ae72f7
Noel Bryant <noelsobryant@gmail.com>
2023-08-16 12:40:28 +0300

    add functionality to save form data to local storage

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const bookTitle = document.getElementById("title");
+document.addEventListener('DOMContentLoaded', () => {
+  const bookTitle = document.getElementById('title');
   const bookAuthor = document.getElementById('author');
   const article = document.getElementById('bookList');
 
   const addButton = document.getElementById('add');
+  const saveItemToLocalStorage = (title, author) => {
+    const savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
+    savedBooks.push({ title, author });
+    localStorage.setItem('savedBooks', JSON.stringify(savedBooks));
+  };
+
+  const removeItemFromLocalStorage = (div) => {
+    const savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
+    const title = div.querySelector('p:first-child').innerHTML;
+    const author = div.querySelector('p:nth-child(2)').innerHTML;
+    const index = savedBooks.findIndex(
+      (item) => item.title === title && item.author === author
+    );
+    if (index !== -1) {
+      savedBooks.splice(index, 1);
+      localStorage.setItem('savedBooks', JSON.stringify(savedBooks));
+    }
+  };
 
   const addTask = (event) => {
     event.preventDefault();
-    if (bookTitle.value === "" || bookAuthor.value === "") {
+    if (bookTitle.value === '' || bookAuthor.value === '') {
       alert('Please input some text in the Author and Title input.');
     } else {
       const div = document.createElement('div'); // Create a new list item
@@ -15,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       const Title = document.createElement('p');
       const Author = document.createElement('p');
-      const hr = document.createElement('hr')
+      const hr = document.createElement('hr');
 
       Title.innerHTML = bookTitle.value;
       Author.innerHTML = bookAuthor.value;
@@ -31,17 +49,14 @@ document.addEventListener('DOMContentLoaded', function () {
       div.appendChild(Title);
       div.appendChild(Author);
       div.appendChild(btn); // Append the remove button
-      div.appendChild(hr)
+      div.appendChild(hr);
 
       article.appendChild(div);
-      bookTitle.value = "";
-      bookAuthor.value = "";
+      bookTitle.value = '';
+      bookAuthor.value = '';
 
       saveItemToLocalStorage(Title.innerHTML, Author.innerHTML);
     }
-  }
-
-
+  };
+  addButton.addEventListener('click', addTask);
 });
-
-
