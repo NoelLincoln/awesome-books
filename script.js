37b05245c742ffcae3d7bccbe078a6851b878370
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-08-17 22:02:10 +0300

    Add constructor function

--- script.js
+++ script.js
@@ -1,91 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bookTitle = document.getElementById('title');
-  const bookAuthor = document.getElementById('author');
-  const article = document.getElementById('bookList');
+class BookManager {
+  constructor () {
+    this.savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
+    this.bookTitle = document.getElementById('title');
+    this.bookAuthor = document.getElementById('author');
+    this.article = document.getElementById('bookList');
+    this.addButton = document.getElementById('add');
 
-  const addButton = document.getElementById('add');
+    this.addButton.addEventListener('click', this.handleOnClickAddBook.bind(this));
+    this.renderBooks();
+  }
 
-  const generateUniqueId = () => {
-    const uniqId = Date.now().toString(36) + Math.random().toString(36).substr(2, 9);
-    return uniqId;
-  };
+ 
+}
 
-  const saveItemToLocalStorage = (id, title, author) => {
-    const savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
-    const updatedBooks = savedBooks.concat({ id, title, author });
-    localStorage.setItem('savedBooks', JSON.stringify(updatedBooks));
-  };
+function initializeApp () {
+  const app = new BookManager();
+}
 
-  const removeItemFromLocalStorage = (id) => {
-    const savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
-    const index = savedBooks.findIndex((item) => item.id === id);
-    if (index !== -1) {
-      const updatedBooks = savedBooks
-        .slice(0, index)
-        .concat(savedBooks.slice(index + 1));
-      localStorage.setItem('savedBooks', JSON.stringify(updatedBooks));
-    }
-  };
-
-  const createBookDiv = (book) => {
-    const div = document.createElement('div');
-    div.className = 'list';
-
-    const Title = document.createElement('p');
-    Title.innerHTML = book.title;
-
-    const Author = document.createElement('p');
-    Author.innerHTML = book.author;
-
-    const btn = document.createElement('button');
-    btn.className = 'remove';
-    btn.innerHTML = 'remove';
-    btn.addEventListener('click', () => {
-      article.removeChild(div);
-      removeItemFromLocalStorage(book.id);
-    });
-
-    const hr = document.createElement('hr');
-
-    div.appendChild(Title);
-    div.appendChild(Author);
-    div.appendChild(btn);
-    div.appendChild(hr);
-
-    return div;
-  };
-
-  const populateFromLocalStorage = () => {
-    const savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
-    savedBooks.forEach((book) => {
-      const div = createBookDiv(book);
-      article.appendChild(div);
-    });
-  };
-
-  const addTask = (event) => {
-    event.preventDefault();
-
-    const id = generateUniqueId();
-    const div = createBookDiv({
-      id,
-      title: bookTitle.value,
-      author: bookAuthor.value,
-    });
-
-    article.appendChild(div);
-    bookTitle.value = '';
-    bookAuthor.value = '';
-
-    saveItemToLocalStorage(
-      id,
-      div.querySelector('p:first-child').innerHTML,
-      div.querySelector('p:nth-child(2)').innerHTML,
-    );
-  };
-
-  addButton.addEventListener('click', addTask);
-
-  // Populate the page from local storage when the page loads
-  populateFromLocalStorage();
-});
+document.addEventListener('DOMContentLoaded', initializeApp);
