6aa342808a1f2d97f73021c0c34de0d92ddc37d7
Noel Bryant <noelsobryant@gmail.com>
2023-08-17 22:33:13 +0300

    add handleOnClickAddBook method

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 class BookManager {
-  constructor () {
+  constructor() {
     this.savedBooks = JSON.parse(localStorage.getItem('savedBooks')) || [];
     this.bookTitle = document.getElementById('title');
     this.bookAuthor = document.getElementById('author');
     this.article = document.getElementById('bookList');
     this.addButton = document.getElementById('add');
 
-    this.addButton.addEventListener('click', this.handleOnClickAddBook.bind(this));
+    this.addButton.addEventListener(
+      'click',
+      this.handleOnClickAddBook.bind(this)
+    );
     this.renderBooks();
   }
 
- 
+  handleOnClickAddBook(event) {
+    event.preventDefault();
+    this.addBook();
+    this.renderBooks();
+    this.bookTitle.value = '';
+    this.bookAuthor.value = '';
+  }
 }
 
-function initializeApp () {
+function initializeApp() {
   const app = new BookManager();
+  return app;
 }
 
 document.addEventListener('DOMContentLoaded', initializeApp);
