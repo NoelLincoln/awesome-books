1d4b15811d085262d39f3c46bfb8db10f1ab4506
Noel Bryant <noelsobryant@gmail.com>
2023-08-19 00:15:21 +0300

    add styling to layout

--- script.js
+++ script.js
@@ -9,41 +9,40 @@ class BookManager {
 
     this.addButton.addEventListener(
       'click',
-      this.handleOnClickAddBook.bind(this),
+      this.handleOnClickAddBook.bind(this)
     );
     this.renderBooks();
   }
 
-  navHandler () {
-    const bookContainer = document.getElementById('books-container')
+  navHandler() {
+    const bookContainer = document.getElementById('books-container');
     const formSection = document.getElementById('form-section');
-    const contactSection = document.getElementById('contact-section')
-    const navUl = document.getElementById("nav_links");
-    const navLinks = navUl.getElementsByClassName("link");
+    const contactSection = document.getElementById('contact-section');
+    const navUl = document.getElementById('nav_links');
+    const navLinks = navUl.getElementsByClassName('link');
     for (let i = 0; i < navLinks.length; i += 1) {
-      navLinks[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
+      navLinks[i].addEventListener('click', function () {
+        const current = document.getElementsByClassName('active');
         if (current.length > 0) {
-          current[0].className = current[0].className.replace(" active", "");
+          current[0].className = current[0].className.replace(' active', '');
         }
-        this.className += " active";
-        if (navLinks[i].innerHTML == 'List') {
+        this.className += ' active';
+        if (navLinks[i].innerHTML === 'List') {
           formSection.style.display = 'none';
           contactSection.style.display = 'none';
-        }else if (navLinks[i].innerHTML == 'Add New') {
-        formSection.style.display = 'block';
-        contactSection.style.display = 'none';
-        bookContainer.style.display = 'none'
-      }else if (navLinks[i].innerHTML == 'Contact') {
-        contactSection.style.display = 'block';
-        formSection.style.display = 'none';
-        bookContainer.style.display = 'none';
+        } else if (navLinks[i].innerHTML == 'Add New') {
+          formSection.style.display = 'block';
+          contactSection.style.display = 'none';
+          bookContainer.style.display = 'none';
+        } else if (navLinks[i].innerHTML === 'Contact') {
+          contactSection.style.display = 'block';
+          formSection.style.display = 'none';
+          bookContainer.style.display = 'none';
         } else {
           bookContainer.style.display = 'block';
           contactSection.style.display = 'none';
           formSection.style.display = 'none';
         }
-
       });
     }
   }
@@ -56,17 +55,21 @@ class BookManager {
     this.bookAuthor.value = '';
   }
 
-  
-
   // add current time method
 
-  updateCurrentTime () {
+  updateCurrentTime() {
     const now = new Date();
-    const options = {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true};
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    };
     const formattedTime = now.toLocaleString('en-US', options);
     this.time.textContent = formattedTime;
   }
-
 
   addBook() {
     const title = this.bookTitle.value.trim();
@@ -116,7 +119,6 @@ function initializeApp() {
 
   document.getElementById('form-section').style.display = 'none';
   document.getElementById('contact-section').style.display = 'none';
-
 
   // Call the updateCurrentTime function initially
   app.updateCurrentTime();
